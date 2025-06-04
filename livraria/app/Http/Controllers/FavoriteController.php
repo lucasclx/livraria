599ea68e8982dc2a103964fb9eb89517430b5da3
Lucas Barbosa Calixto <81984351+lucasclx@users.noreply.github.com>
2025-06-04 17:23:12 -0300

    Add favorites feature

---
+++ livraria/app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Livro;
+use Illuminate\Support\Facades\Auth;
+
+class FavoriteController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function toggle(Livro $livro)
+    {
+        $user = Auth::user();
+
+        if ($user->favorites()->where('livro_id', $livro->id)->exists()) {
+            $user->favorites()->detach($livro->id);
+            $favorited = false;
+        } else {
+            $user->favorites()->attach($livro->id);
+            $favorited = true;
+        }
+
+        return response()->json(['favorited' => $favorited]);
+    }
+}
